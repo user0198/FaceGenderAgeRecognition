cc7083c5879c7186f437e3d46eff5a8636d3b869
John Doe <jecka0198@gmail.com>
2020-11-24 20:23:15 +0200

    refactored

--- face_gender_age_recognition.py
+++ face_gender_age_recognition.py
@@ -1,3 +1,6 @@
+# Face, Gender and Age recognition in real time from webcam capture
+# Eugene Kykyna
+
 import face_recognition
 import numpy as np
 import cv2
@@ -6,7 +9,7 @@ import argparse
 import os
 
 
-# Draws frame around detected face
+# Draws a rectangle around face
 def highlightFace(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
     frameHeight = frameOpencvDnn.shape[0]
@@ -40,28 +43,25 @@ def findEncodings(images):
     return encodeList
 
 
-# Get names from the images
+# Path to known faces to recognize
 path = 'resources/known'
-images = []
-known_face_names = []
 pathList = os.listdir(path)
 
-for im in pathList:
-    curImg = cv2.imread(f'{path}/{im}')
-    images.append(curImg)
-    known_face_names.append(os.path.splitext(im)[0])
+# List of known faces images
+images = []
 
-# Create arrays of known face encodings
-known_face_encodings = findEncodings(images)
+# List of names from images
+knownFaceNames = []
 
-# Initialize current frame marker
-process_this_frame = True
+# Distance between face box and rectangle to draw
+padding = 20
 
+# Argparse init
 parser = argparse.ArgumentParser()
 parser.add_argument('--image')
-
 args = parser.parse_args()
 
+# Import from neural network database
 faceProto = "opencv_face_detector.pbtxt"
 faceModel = "opencv_face_detector_uint8.pb"
 ageProto = "age_deploy.prototxt"
@@ -69,21 +69,35 @@ ageModel = "age_net.caffemodel"
 genderProto = "gender_deploy.prototxt"
 genderModel = "gender_net.caffemodel"
 
+# Weights and classifiers
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
            '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
-
+# Reading from deep neural network
 faceNet = cv2.dnn.readNet(faceModel, faceProto)
 ageNet = cv2.dnn.readNet(ageModel, ageProto)
 genderNet = cv2.dnn.readNet(genderModel, genderProto)
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(args.image if args.image else 0)
-padding = 20
+# Gets names from the images
+for im in pathList:
+    curImg = cv2.imread(f'{path}/{im}')
+    images.append(curImg)
+    knownFaceNames.append(os.path.splitext(im)[0])
 
+# Create arrays of known face encodings
+knownFaceEncodings = findEncodings(images)
+
+# Initialize current frame marker
+processThisFrame = True
+
+# Get a reference to webcam
+videoCapture = cv2.VideoCapture(args.image if args.image else 0)
+
+# Main loop
 while cv2.waitKey(1) < 0:
-    hasFrame, frame = video_capture.read()
+    hasFrame, frame = videoCapture.read()
+    # Break loop if face is not in frame
     if not hasFrame:
         cv2.waitKey()
         break
@@ -93,53 +107,62 @@ while cv2.waitKey(1) < 0:
         print("No face detected")
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    smallFrame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    smallerFrame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_smallFrame = cv2.cvtColor(smallFrame, cv2.COLOR_BGR2RGB)
+    rgbSmallerFrame = cv2.cvtColor(smallerFrame, cv2.COLOR_BGR2RGB)
 
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_smallFrame)
-        face_encodings = face_recognition.face_encodings(
-            rgb_smallFrame, face_locations)
+    # Check if calculation is being proceed once for a single frame
+    if processThisFrame:
+        # Find all the faces and get face encodings in the current frame
+        faceLocations = face_recognition.face_locations(rgbSmallerFrame)
+        faceEncodings = face_recognition.face_encodings(
+            rgbSmallerFrame, faceLocations)
 
-        face_names = []
-        for face_encode, face_loc in zip(face_encodings, face_locations):
-            # See if the face is a match for the known face(s)
+        facesNames = []
+        for faceEncode, _ in zip(faceEncodings, faceLocations):
+            # See if face mathes known one
             matches = face_recognition.compare_faces(
-                known_face_encodings, face_encode)
-            name = "-"
+                knownFaceEncodings, faceEncode)
+            # Leave blank if no match
+            name = ""
 
-            # Use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(
-                known_face_encodings, face_encode)
+            # Get euclid distance between known faces encodings and current one in a frame
+            faceDistances = face_recognition.face_distance(
+                knownFaceEncodings, faceEncode)
 
-            best_match_index = np.argmin(face_distances)
+            # Calculate best match index
+            bestMatchIndex = np.argmin(faceDistances)
 
-            if matches[best_match_index]:
-                name = known_face_names[best_match_index]
-            face_names.append(name)
+            # Connect name to an encoded face in a frame
+            if matches[bestMatchIndex]:
+                name = knownFaceNames[bestMatchIndex]
+            facesNames.append(name)
 
-    process_this_frame = not process_this_frame
+    # Move to the next frame
+    processThisFrame = not processThisFrame
 
     for faceBox in faceBoxes:
+        # Get face cut from frame
         face = frame[max(0, faceBox[1]-padding):
                      min(faceBox[3]+padding, frame.shape[0]-1), max(0, faceBox[0]-padding):min(faceBox[2]+padding, frame.shape[1]-1)]
 
         blob = cv2.dnn.blobFromImage(
             face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+
+        # Predict gender
         genderNet.setInput(blob)
         genderPreds = genderNet.forward()
         gender = genderList[genderPreds[0].argmax()]
         print(f'Gender: {gender}')
 
+        # Predict age
         ageNet.setInput(blob)
         agePreds = ageNet.forward()
         age = ageList[agePreds[0].argmax()]
         print(f'Age: {age[1:-1]} years')
 
-        # Draws name
-        for (top, _, _, left), name in zip(face_locations, face_names):
+        # Draw name
+        for (top, _, _, left), name in zip(faceLocations, facesNames):
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
             top *= 4
             # right *= 4
@@ -150,12 +173,14 @@ while cv2.waitKey(1) < 0:
             cv2.putText(resultImg, name, (left, top - 70),
                         cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 0), 1, cv2.LINE_AA)
 
-        # Draws gender and age
+        # Draw gender and age
         cv2.putText(resultImg, f'{gender}, {age}', (
             faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 255, 0), 1, cv2.LINE_AA)
 
+        # Display drawings
         cv2.imshow("Detecting ...", resultImg)
 
 
-video_capture.release()
+# Release memory
+videoCapture.release()
 cv2.destroyAllWindows()
